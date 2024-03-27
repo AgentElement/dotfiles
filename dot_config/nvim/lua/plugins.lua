c424ec16533de34112c69ed0b325ae99946980c3
AgentElement <agentelement@protonmail.com>
2024-03-27 09:20:00 -0700

    packer.nvim is unmaintained, move to lazy.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,136 +1,145 @@
-return require('packer').startup(function(use)
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+
+require("lazy").setup({
+    
     ---------------------------------------------------------------------------
     -- PACKER
-    use 'https://github.com/wbthomason/packer.nvim'
+    'https://github.com/wbthomason/packer.nvim',
 
     ---------------------------------------------------------------------------
     -- VISUALS
 
     -- Color scheme
-    use 'https://github.com/navarasu/onedark.nvim'
+    'https://github.com/navarasu/onedark.nvim',
 
     -- Icons
-    use 'https://github.com/kyazdani42/nvim-web-devicons'
-    use 'https://github.com/ryanoasis/vim-devicons'
+    'https://github.com/kyazdani42/nvim-web-devicons',
+    'https://github.com/ryanoasis/vim-devicons',
 
     -- View indentation guides
-    use 'https://github.com/lukas-reineke/indent-blankline.nvim'
+    'https://github.com/lukas-reineke/indent-blankline.nvim',
 
     -- Status bar
-    use 'https://github.com/nvim-lualine/lualine.nvim'
+    'https://github.com/nvim-lualine/lualine.nvim',
 
     -- Smooth scrolling
-    -- use 'https://github.com/psliwka/vim-smoothie'
+    -- 'https://github.com/psliwka/vim-smoothie'
 
     -- Semantic highlighting
-    use 'https://github.com/RRethy/vim-illuminate'
+    'https://github.com/RRethy/vim-illuminate',
 
     -- Colors
-    use 'https://github.com/brenoprata10/nvim-highlight-colors'
+    'https://github.com/brenoprata10/nvim-highlight-colors',
 
     ---------------------------------------------------------------------------
-    -- GIT
+    -- GI
 
     -- Main git wrapper
-    use 'https://github.com/tpope/vim-fugitive'
+    'https://github.com/tpope/vim-fugitive',
 
     -- Show most recent commit
-    use 'https://github.com/rhysd/git-messenger.vim'
+    'https://github.com/rhysd/git-messenger.vim',
 
     -- Git diff in sign column
-    use 'https://github.com/airblade/vim-gitgutter'
+    'https://github.com/airblade/vim-gitgutter',
 
 
     ---------------------------------------------------------------------------
     -- UTILITIES
 
     -- Powerful file picker
-    use {
+    {
         'https://github.com/nvim-telescope/telescope.nvim',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
     -- Floating terminal
-    use 'https://github.com/voldikss/vim-floaterm'
+    'https://github.com/voldikss/vim-floaterm',
 
     -- Better commenting
-    use 'https://github.com/preservim/nerdcommenter'
+    'https://github.com/preservim/nerdcommenter',
 
     -- Neovim sidebar file manager
-    use {
+    {
         'https://github.com/kyazdani42/nvim-tree.lua',
-        requires = 'https://github.com/kyazdani42/nvim-web-devicons',
-    }
+        dependencies = { 'https://github.com/kyazdani42/nvim-web-devicons' },
+    },
 
     -- Improved tab bar
-    use 'https://github.com/romgrk/barbar.nvim'
+    'https://github.com/romgrk/barbar.nvim',
 
     -- Autoformatting
-    use 'https://github.com/Chiel92/vim-autoformat'
+    'https://github.com/Chiel92/vim-autoformat',
 
     -- Startup screen
-    use 'https://github.com/mhinz/vim-startify'
+    'https://github.com/mhinz/vim-startify',
 
     -- ASCII diagrams in vim
-    use 'https://github.com/jbyuki/venn.nvim'
+    'https://github.com/jbyuki/venn.nvim',
 
     -- Better / searching
-    use 'https://github.com/junegunn/vim-slash'
+    'https://github.com/junegunn/vim-slash',
 
     ---------------------------------------------------------------------------
     -- LANGUAGE
 
     -- Treesitter
-    use {
+    {
         'https://github.com/nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+        build = ':TSUpdate'
+    },
 
     -- LSP installer
-    use "https://github.com/williamboman/mason.nvim"
+    "https://github.com/williamboman/mason.nvim",
 
     -- mason/lspconfig bridge
-    use "https://github.com/williamboman/mason-lspconfig.nvim"
+    "https://github.com/williamboman/mason-lspconfig.nvim",
 
     -- LSP configuration
-    use "https://github.com/neovim/nvim-lspconfig"
+    "https://github.com/neovim/nvim-lspconfig",
 
     -- Formatter
-    use "https://github.com/mhartington/formatter.nvim"
+    "https://github.com/mhartington/formatter.nvim",
 
-    -- Use virtual text for warnings
-    use 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
+    -- virtual text for warnings
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
 
     -- Completion
-    use 'https://github.com/hrsh7th/cmp-nvim-lsp'
-    use 'https://github.com/hrsh7th/cmp-cmdline'
-    use 'https://github.com/hrsh7th/cmp-buffer'
-    use 'https://github.com/hrsh7th/cmp-path'
-    use 'https://github.com/hrsh7th/nvim-cmp'
+    'https://github.com/hrsh7th/cmp-nvim-lsp',
+    'https://github.com/hrsh7th/cmp-cmdline',
+    'https://github.com/hrsh7th/cmp-buffer',
+    'https://github.com/hrsh7th/cmp-path',
+    'https://github.com/hrsh7th/nvim-cmp',
 
     -- Luasnip completion source for cmp
-    use 'https://github.com/saadparwaiz1/cmp_luasnip'
+    'https://github.com/saadparwaiz1/cmp_luasnip',
 
     -- Snippets
-    use 'https://github.com/L3MON4D3/LuaSnip'
+    'https://github.com/L3MON4D3/LuaSnip',
 
     -- OpenSCAD extension
-    use 'https://github.com/salkin-mada/openscad.nvim'
+    'https://github.com/salkin-mada/openscad.nvim',
 
 
     -- LaTeX suite
-    -- use 'https://github.com/lervag/vimtex'
+    -- 'https://github.com/lervag/vimtex'
 
     -- C++ clangd_extensions
-    use 'https://github.com/p00f/clangd_extensions.nvim'
+    'https://github.com/p00f/clangd_extensions.nvim',
 
     -- Rust-tools
-    use 'https://github.com/simrat39/rust-tools.nvim'
-
-    -- Automatically set up configuration after cloning packer.nvim
-    -- Always to be put after plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+    'https://github.com/simrat39/rust-tools.nvim',
+})
