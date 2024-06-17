21107a1dc2476eb978221922153146c675e1753a
agentelement <agentelement@protonmail.com>
2024-06-17 02:20:41 -0700

    Clean up fw13 config args

--- hardware/framework13/configuration.nix
+++ hardware/framework13/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ inputs, config, lib, pkgs, ... }:
+{pkgs, ... }:
 
 {
   imports =
@@ -173,6 +173,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
 
