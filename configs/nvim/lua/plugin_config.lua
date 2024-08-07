97c8fd6d72eb7310446215b6621fdd31b7792aac
agentelement <agentelement@protonmail.com>
2024-08-06 21:41:55 -0700

    nvim: remove old plugin configs, add lean config

--- configs/nvim/lua/plugin_config.lua
+++ configs/nvim/lua/plugin_config.lua
@@ -6,34 +6,6 @@ require('onedark').setup {
 }
 require('onedark').load()
 
-require('gen').setup({
-    opts = {
-        model = "llama3",    -- The default model to use.
-        host = "localhost",  -- The host running the Ollama service.
-        port = "11434",      -- The port on which the Ollama service is listening.
-        quit_map = "q",      -- set keymap for close the response window
-        retry_map = "<c-r>", -- set keymap to re-send the current prompt
-        init = function(options) end,
-        -- Function to initialize Ollama
-        command = function(options)
-            local body = { model = options.model, stream = true }
-            return "curl --silent --no-buffer -X POST http://" ..
-                options.host .. ":" .. options.port .. "/api/chat -d $body"
-        end,
-        -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-        -- This can also be a command string.
-        -- The executed command must return a JSON object with { response, context }
-        -- (context property is optional).
-        -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-        display_mode = "split", -- The display mode. Can be "float" or "split".
-        show_prompt = true,     -- Shows the prompt submitted to Ollama.
-        show_model = true,      -- Displays which model you are using at the beginning of your chat session.
-        no_auto_close = false,  -- Never closes the window automatically.
-        debug = false           -- Prints errors and the command which is run.
-    }
-})
-
-
 require('illuminate')
 
 require('nvim-highlight-colors').setup {}
@@ -71,14 +43,45 @@ require('nvim-tree').setup {
     },
 }
 
-require('lean').setup { mappings = true }
+require('lean').setup {
+    mappings = true,
+    lsp = {
+        init_options = {
+            -- See Lean.Lsp.InitializationOptions for details and further options.
+
+            -- Time (in milliseconds) which must pass since latest edit until elaboration begins.
+            -- Lower values may make editing feel faster at the cost of higher CPU usage.
+            -- Note that lean.nvim changes the Lean default for this value!
+            editDelay = 0,
+
+            -- Whether to signal that widgets are supported.
+            hasWidgets = true,
+        }
+    },
+
+    stderr = {
+        enable = true,
+        -- height of the window
+        height = 5,
+        -- a callback which will be called with (multi-line) stderr output
+        -- e.g., use:
+        --   on_lines = function(lines) vim.notify(lines) end
+        -- if you want to redirect stderr to `vim.notify`.
+        -- The default implementation will redirect to a dedicated stderr
+        -- window.
+        on_lines = nil,
+    },
+}
 
 -- lsp_lines
 -- require("lsp_lines").register_lsp_virtual_lines()
 
 -- treesitter
 require('nvim-treesitter.configs').setup({
+    -- Ensure all treesitter parsers are installed
     ensure_installed = "all",
+    
+    -- enable highlighting and disable vim's default regex-based highlighting
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
@@ -88,35 +91,11 @@ require('nvim-treesitter.configs').setup({
 
 -- toggleterm
 require('toggleterm').setup {
+    -- terminal size is 0.4 * window height
     size = vim.o.lines * 0.4,
     shade_terminals = false,
     start_in_insert = false,
 }
-
---
--- vimtex
-vim.g.vimtex_compiler_progname = 'nvr'
-vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_general_viewer = 'okular'
--- vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
--- vim.g.vimtex_view_general_options_latexmk = '--unique'
-
--- startify
-vim.g.startify_session_persistence = true
-
--- NERDCommenter
--- Add spaces after comment delimiters by default
-vim.g.NERDSpaceDelims = true
-vim.g.NERDDefaultAlign = 'left'
-
--- Allow commenting and inverting empty lines (useful when commenting a region)
-vim.g.NERDCommentEmptyLines = true
-
--- Enable trimming of trailing whitespace when uncommenting
-vim.g.NERDTrimTrailingWhitespace = true
-
--- Do not create default mappings
-vim.g.NERDCreateDefaultMappings = false
 
 -- OpenSCAD
 require('openscad')
