c6cb42770db39a98ab76c89280072382d432dbc8
agentelement <agentelement@agentelement.net>
2025-11-17 17:52:48 -0700

    nix: clean up framework desktop config

--- hardware/frameworkdesktop/configuration.nix
+++ hardware/frameworkdesktop/configuration.nix
@@ -28,7 +28,6 @@
     "amd_iommu=off"
     "amdgpu.gttsize=108000"
     "ttm.pages_limit=33554432"
-    "amdgpu.ppfeaturemask=0xf7fff"
   ];
   boot.kernelPackages = lib.recurseIntoAttrs (pkgs.linuxPackagesFor (pkgs.buildLinux {
     version = "6.18-rc5";
@@ -40,10 +39,6 @@
   networking.networkmanager.enable = true;
 
   time.timeZone = "America/Phoenix";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -133,6 +128,7 @@
     enable = true;
     withUWSM = true;
   };
+
   programs.uwsm.enable = true;
   programs.uwsm.waylandCompositors.hyprland = {
     prettyName = "Hyprland";
@@ -140,22 +136,6 @@
     binPath = "/run/current-system/sw/bin/Hyprland";
   };
 
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -189,18 +169,6 @@
   services.mullvad-vpn.enable = true;
   nixpkgs.config.rocmSupport = true;
 
-
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
