6c86cf0ea9a0c875397f1d93449fca05f1c2a9e8
agentelement <agentelement@agentelement.net>
2026-05-21 21:37:56 -0700

    nix: framework server config

---
+++ hardware/frameworkserver/configuration.nix
@@ -0,0 +1,168 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  pkgs,
+  lib,
+  outputs,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./homelab.nix
+    ../syncthing.nix
+  ];
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+    ];
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.enableCryptodisk = true;
+  boot.loader.grub.efiSupport = true;
+
+  boot.kernelParams = [
+    "ttm.page_pool_size=29696000"
+    "ttm.pages_limit=29696000"
+    "amdgpu.runpm=0"
+    "pcie_aspm=off"
+    "pcie_port_pm=off"
+    "nvme_core.default_ps_max_latency_us=0"
+  ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "lambda";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Phoenix";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    # keyMap = "us";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable sound
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.groups.storage = { };
+  users.groups.uinput = { };
+
+  hardware.uinput.enable = true;
+
+  programs.zsh.enable = true;
+
+  users.users.agentelement = {
+    isNormalUser = true;
+    extraGroups = [
+      "dialout"
+      "wheel"
+      "video"
+      "storage"
+      "networkmanager"
+      "input"
+      "uinput"
+      "podman"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    gcc
+    pulseaudio
+    gnupg
+    podman
+    podman-compose
+  ];
+
+  # Beware, proprietary garbage here.
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-original"
+      "steam-run"
+      "steam-unwrapped"
+    ];
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-tty;
+  };
+
+  # Thunderbolt daemon
+  services.hardware.bolt.enable = true;
+
+  # Query and manipulate storage devices
+  services.udisks2.enable = true;
+
+  # Update firmware
+  services.fwupd.enable = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  programs.uwsm.enable = true;
+  programs.jai-jail.enable = true;
+  programs.uwsm.waylandCompositors.hyprland = {
+    prettyName = "Hyprland";
+    comment = "Hyprland compositor managed by UWSM";
+    binPath = "/run/current-system/sw/bin/Hyprland";
+  };
+
+  services.mullvad-vpn.enable = true;
+  nixpkgs.config.rocmSupport = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hardware/frameworkserver/homelab.nix
@@ -0,0 +1,83 @@
+{
+  pkgs,
+  lib,
+  config,
+  self,
+  ...
+}:
+{
+  services.llama-cpp = {
+    enable = true;
+    package = (
+      pkgs.llama-cpp.override {
+        rocmSupport = true;
+      }
+    );
+    host = "0.0.0.0";
+    modelsDir = "/storage/models/";
+    extraFlags = [
+      "--no-mmap"
+      "-ngl"
+      "999"
+      "-fa"
+      "1"
+      "--ctx-size"
+      "0"
+    ];
+    port = 8080;
+  };
+
+  environment.systemPackages = with pkgs; [
+    (llama-cpp.override {
+      rocmSupport = true;
+    })
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "agentelement" ];
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
+    knownHosts = {
+      delta = {
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgiDj6ZFvu7Ta/4ZQ+c3JZlw/lTj5j3dmVqr11YksFz agentelement@delta";
+      };
+    };
+  };
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  services.immich = {
+    enable = true;
+    port = 2283;
+  };
+
+  services.invidious = {
+    enable = true;
+    port = 3000;
+  };
+
+  services.caddy = {
+    enable = true;
+    configFile = ../../configs/caddy/Caddyfile;
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+}
