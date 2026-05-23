3f4557a8a150b702d94ac7d7075ccaa7389b1a8e
agentelement <agentelement@agentelement.net>
2026-05-22 19:31:42 -0700

    caddy: ssl certs for *.local.agentelement.net

--- hardware/frameworkserver/homelab.nix
+++ hardware/frameworkserver/homelab.nix
@@ -82,8 +82,23 @@
 
   sops = {
     age.keyFile = "/home/agentelement/.config/sops/age/keys.txt";
-    defaultSopsFile = "../configs/secrets/secrets.yaml";
+    defaultSopsFile = ../../configs/secrets/secrets.yaml;
   };
+
+  sops.secrets."namecheap/api_key" = {};
+  sops.secrets."namecheap/api_user" = {};
+  sops.secrets."namecheap/client_ip" = {};
+  sops.templates."namecheap.env" = {
+    content = ''
+      NAMECHEAP_API_KEY=${config.sops.placeholder."namecheap/api_key"}
+      NAMECHEAP_API_USER=${config.sops.placeholder."namecheap/api_user"}
+      NAMECHEAP_CLIENT_IP=${config.sops.placeholder."namecheap/client_ip"}
+    '';
+    owner = config.services.caddy.user;
+    group = config.services.caddy.group;
+  };
+
+  systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates."namecheap.env".path;
 
   networking.firewall.interfaces."wg-homelab".allowedTCPPorts = [
     80
