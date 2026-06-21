38792753dfb9a137027a86d6961f3b7bd20c296b
agentelement <agentelement@agentelement.net>
2026-06-20 20:28:36 -0700

    nix: llama.cpp rpc server on theta

--- hardware/frameworkdesktop/configuration.nix
+++ hardware/frameworkdesktop/configuration.nix
@@ -112,6 +112,27 @@
 
   nixpkgs.config.rocmSupport = true;
 
+  environment.systemPackages = [
+    (pkgs.llama-cpp.override {
+      rocmSupport = true;
+      rpcSupport = true;
+    })
+  ];
+
+  systemd.services.llama-rpc-server = {
+    enable = true;
+    description = "llama.cpp rpc";
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "/run/current-system/sw/bin/llama-rpc-server --host 10.0.0.2 -c -p 50052";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    50052
+  ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
